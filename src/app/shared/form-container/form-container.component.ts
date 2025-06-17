c14f2ebdbd84b4c5871bd171f7d32d4159501c7b
chitturi prasanna <chitturi.prasanna@coditas.com>
2025-06-17 15:48:56 +0530

    refresh feature

--- src/app/shared/form-container/form-container.component.ts
+++ src/app/shared/form-container/form-container.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatStepperModule } from '@angular/material/stepper';
 import { SignUpComponent } from '../../pages/sign-up/sign-up.component';
 import { PersonalInfomationComponent } from '../../pages/personal-infomation/personal-infomation.component';
 import { EducationInfomationComponent } from '../../pages/education-infomation/education-infomation.component';
+import { StepperSelectionEvent } from '@angular/cdk/stepper';
 
 @Component({
   selector: 'app-form-container',
@@ -10,6 +11,17 @@ import { EducationInfomationComponent } from '../../pages/education-infomation/e
   templateUrl: './form-container.component.html',
   styleUrl: './form-container.component.scss'
 })
-export class FormContainerComponent {
+export class FormContainerComponent implements OnInit{
+  selectedIndex = 0;
+  ngOnInit() {
+    const storedStep = localStorage.getItem('activeStepIndex');
+    if (storedStep !== null) {
+      this.selectedIndex = +storedStep;
+    }
+  }
+
+  onStepChange(event: StepperSelectionEvent) {
+    localStorage.setItem('activeStepIndex', event.selectedIndex.toString());
+  }
   isLinear = true;
 }
